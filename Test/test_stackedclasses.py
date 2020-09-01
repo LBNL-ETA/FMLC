a86b823fc3be307767e9829cb85f6c8819cef6ae
Linyue-Song <oliver@picovoice.ai>
2020-09-01 02:18:40 -0700

    added more tests

--- Test/test_stackedclasses.py
+++ Test/test_stackedclasses.py
@@ -55,7 +55,7 @@ class testcontroller4(eFMU):
         time.sleep(10)
 
 def test_sampletime():
-    print("Testing sample time:")
+    '''This tests if the sample time is working properly'''
     controller = {}
     controller['forecast1'] = {'fun': testcontroller1, 'sampletime': 3}
     controller = controller_stack(controller, tz=-8, debug=True, parallel=True)
@@ -72,9 +72,47 @@ def test_sampletime():
     for i in (np.diff(df.index) / np.timedelta64(1, 's'))[1:]:
         assert(math.isclose(i, 3, rel_tol=0.01))
 
+def test_normal():
+    controller = {}
+    controller['forecast1'] = {'fun':testcontroller1, 'sampletime':0}
+    controller['mpc1'] = {'fun':testcontroller2, 'sampletime':'forecast1'}
+    controller['control1'] = {'fun':testcontroller1, 'sampletime':'mpc1'}
+    controller['forecast2'] = {'fun':testcontroller3, 'sampletime':0}
+    controller['forecast3'] = {'fun':testcontroller1, 'sampletime':0}
+    controller = controller_stack(controller, tz=-8, debug=True, parallel=True, timeout=2)
+
+    mapping = {}
+    mapping['forecast1_a'] = 10
+    mapping['forecast1_b'] = 4
+    mapping['forecast2_a'] = 20
+    mapping['forecast2_b'] = 4
+    mapping['forecast3_a'] = 30
+    mapping['forecast3_b'] = 4
+    mapping['mpc1_a'] = 'forecast1_c'
+    mapping['mpc1_b'] = 'forecast1_a'
+    mapping['control1_a'] = 'mpc1_c'
+    mapping['control1_b'] = 'mpc1_a'
+    controller.initialize(mapping)
+
+    for i in range(6):
+        controller.query_control(time.time())
+        time.sleep(1.5)
+    
+    df1 = pd.DataFrame(controller.log_to_df()['forecast1'])
+    df2 = pd.DataFrame(controller.log_to_df()['forecast2'])
+    df3 = pd.DataFrame(controller.log_to_df()['forecast3'])
+    df4 = pd.DataFrame(controller.log_to_df()['mpc1'])
+    df5 = pd.DataFrame(controller.log_to_df()['control1'])
+
+    assert df1.shape[0] == 3
+    assert df2.shape[0] == 4
+    assert df3.shape[0] == 4
+    assert df4.shape[0] == 3
+    assert df5.shape[0] == 2 or df5.shape[0] == 3
+
 def test_stuckController():
-    print("Testing stuck controllers:")
-    ## mpc1 stuck
+    '''This tests if the timeout controllers can be caught'''
+    ## CASE1: mpc1 stuck
     controller = {}
     controller['forecast1'] = {'fun':testcontroller1, 'sampletime':0}
     controller['mpc1'] = {'fun':testcontroller4, 'sampletime':'forecast1'}
@@ -95,7 +133,7 @@ def test_stuckController():
     mapping['control1_a'] = 'mpc1_c'
     mapping['control1_b'] = 'mpc1_a'
     controller.initialize(mapping)
-
+    
     for i in range(3):
         controller.query_control(time.time())
         time.sleep(1)
@@ -116,4 +154,43 @@ def test_stuckController():
     assert controller.executed_controllers.__str__() == "['forecast2', 'forecast3']"
     assert controller.timeout_controllers.__str__() == '[]'
 
+    ##CASE2: mpc1 and forcast2 stuck
+    controller = {}
+    controller['forecast1'] = {'fun':testcontroller1, 'sampletime':0}
+    controller['mpc1'] = {'fun':testcontroller3, 'sampletime':'forecast1'}
+    controller['control1'] = {'fun':testcontroller1, 'sampletime':'mpc1'}
+    controller['forecast2'] = {'fun':testcontroller3, 'sampletime':0}
+    controller['forecast3'] = {'fun':testcontroller1, 'sampletime':0}
+    controller = controller_stack(controller, tz=-8, debug=True, parallel=True, timeout=0.8)
+
+    mapping = {}
+    mapping['forecast1_a'] = 10
+    mapping['forecast1_b'] = 4
+    mapping['forecast2_a'] = 20
+    mapping['forecast2_b'] = 4
+    mapping['forecast3_a'] = 30
+    mapping['forecast3_b'] = 4
+    mapping['mpc1_a'] = 'forecast1_c'
+    mapping['mpc1_b'] = 'forecast1_a'
+    mapping['control1_a'] = 'mpc1_c'
+    mapping['control1_b'] = 'mpc1_a'
+    controller.initialize(mapping)
+
+    for i in range(6):
+        controller.query_control(time.time())
+        time.sleep(1.5)
     
+    df1 = pd.DataFrame(controller.log_to_df()['forecast1'])
+    df2 = pd.DataFrame(controller.log_to_df()['forecast2'])
+    df3 = pd.DataFrame(controller.log_to_df()['forecast3'])
+    df4 = pd.DataFrame(controller.log_to_df()['mpc1'])
+    df5 = pd.DataFrame(controller.log_to_df()['control1'])
+
+    assert df1.shape[0] == 3 
+    assert df2.shape[0] == 1
+    assert df3.shape[0] == 4
+    assert df4.shape[0] == 1
+    assert df5.shape[0] == 1
+    assert len(df2.columns) == 1
+    assert len(df4.columns) == 1
+    assert len(df5.columns) == 1
