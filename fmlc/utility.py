e7e8797f7794cea29242d29e2407b898ea89c85b
Christoph <cgehbauer@lbl.gov>
2023-01-11 14:51:59 -0800

    fixed error when message is not string'

--- fmlc/utility.py
+++ fmlc/utility.py
@@ -5,7 +5,7 @@ def check_error(logs, printing=False):
     errors = pd.DataFrame()
     for n, l in logs.items():
         for t, m in l['logging'].items():
-            if 'error' in m.lower():
+            if 'error' in str(m).lower():
                 errors.loc[i, 'module'] = n
                 errors.loc[i, 'timestep'] = t
                 errors.loc[i, 'message'] = m
