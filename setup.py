3f0cd3db1f49c8871f26c7c8625b356bf0c09ea3
Christoph <cgehbauer@lbl.gov>
2023-04-25 22:52:26 -0700

    updated version

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-    
-with open('requirements.txt') as f:
+# description
+with open('README.md', 'r', encoding='utf8') as f:
+    long_description = f.read()
+
+# requirements
+with open('requirements.txt', 'r', encoding='utf8') as f:
     install_requires = f.read().splitlines()
+
+# version
+with open('fmlc/__init__.py', 'r', encoding='utf8') as f:
+    version = json.loads(f.read().split('__version__ = ')[1].split('\n')[0])
 
 setuptools.setup(
     name="FMLC",
-    version="1.2.1",
+    version=version,
     author="Gehbauer, Christoph",
     description="A framework/backend for multi-layer and multi-time domain controller.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license_files = ['license.txt'],
     url="https://github.com/LBNL-ETA/FMLC",
     project_urls={
         "Bug Tracker": "https://github.com/LBNL-ETA/FMLC/issues",
@@ -21,7 +28,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(),
+    packages=['fmlc'],
     python_requires=">=3.6",
     install_requires= install_requires
 )
